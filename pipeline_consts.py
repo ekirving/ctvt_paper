8ed3afdb2a9b63163bc46cf71a7b7424da9f634f
Evan Irving-Pease <ekirving@hotmail.com>
2017-05-04 10:39:44 +0100

    code cleanup

--- pipeline_consts.py
+++ pipeline_consts.py
@@ -39,23 +39,23 @@ GROUPS = {
                      'DQA', 'DCH', 'DTI', 'DTM', 'DVN', 'DPC', 'CTVT'],
     },
 
-    'merged_v1':        { 'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS},
-    'merged_v1.random': { 'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS },
-    'merged_v1_hq':     { 'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS },
-    'merged_v1_hq2':    { 'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS },
-    'merged_v1_TV':     { 'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS },
-    'merged_v1_TV_hq':  { 'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS },
+    'merged_v1':        {'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS},
+    'merged_v1.random': {'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS},
+    'merged_v1_hq':     {'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS},
+    'merged_v1_hq2':    {'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS},
+    'merged_v1_TV':     {'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS},
+    'merged_v1_TV_hq':  {'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS},
 
-    'merged_v2':        { 'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS },
-    'merged_v2.random': { 'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS },
-    'merged_v2_hq':     { 'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS },
-    'merged_v2_hq2':    { 'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS },
-    'merged_v2_TV':     { 'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS },
-    'merged_v2_TV_hq':  { 'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS },
+    'merged_v2':        {'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS},
+    'merged_v2.random': {'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS},
+    'merged_v2_hq':     {'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS},
+    'merged_v2_hq2':    {'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS},
+    'merged_v2_TV':     {'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS},
+    'merged_v2_TV_hq':  {'all-pops': ALL_POPS, 'dog-ctvt' : ALL_DOGS},
 
-    'merged_SNParray':           { 'all-pops': SNP_ARRAY_POPS },
-    'merged_SNParray_v1':        { 'all-pops': SNP_ARRAY_POPS },
-    'merged_SNParray_v1_noCTVT': { 'all-pops': SNP_ARRAY_POPS },
+    'merged_SNParray':           {'all-pops': SNP_ARRAY_POPS},
+    'merged_SNParray_v1':        {'all-pops': SNP_ARRAY_POPS},
+    'merged_SNParray_v1_noCTVT': {'all-pops': SNP_ARRAY_POPS},
 
 }
 
