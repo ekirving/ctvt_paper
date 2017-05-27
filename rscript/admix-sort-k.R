e68b877afcfef8c8493d79bac7ad8e3662dce6c5
Evan Irving-Pease <ekirving@hotmail.com>
2017-05-27 10:52:41 +0100

    bug fix for identical sorting scores

--- rscript/admix-sort-k.R
+++ rscript/admix-sort-k.R
@@ -10,10 +10,10 @@ kprior=args[2]
 qsorted=args[3]
 
 # TODO remove when done testing
-# setwd("/Users/Evan/Dropbox/Code/socotra")
-# qfile = "admix/all-pops.BovineHD.pruned.10.Q"
-# kprior = "admix/all-pops.BovineHD.pruned.sorted.9.Q"
-# qsorted = "admix/all-pops.BovineHD.pruned.sorted.10.Q"
+# setwd("/Users/Evan/Dropbox/Code/ctvt")
+# qfile = "admix/qpgraph-pops.merged_v2_hq2_nomex_ctvt.pruned.4.Q"
+# kprior = "admix/qpgraph-pops.merged_v2_hq2_nomex_ctvt.pruned.sorted.3.Q"
+# qsorted = "admix/qpgraph-pops.merged_v2_hq2_nomex_ctvt.pruned.sorted.4.Q"
 
 # read the Q matrices into dataframes
 dat1 = read.table(kprior)
@@ -28,50 +28,50 @@ for (i in 1:numPad) {
 # if K < 10 we can test all the permutations to find the absolute best fit
 # this algorithm has complexity of O(n!) (e.g. 10! = 3,628,800 perms)
 if (length(dat2) < 10) {
-  
+
   # make a vector containing all the possible permutations of the column orders
   colPerms <- permn(names(dat2))
-  
+
   sums <- c()
-  
+
   # initialise a parallel cluster
   cl <- makeCluster(detectCores()[1] * 0.5) # only use 50% of the available cores
   registerDoParallel(cl)
-  
+
   # TODO this would be a lot quicker if the job was chunked
   # iterate over every permutation of the matrix in parallel
   sums <- foreach(i = seq_along(colPerms), .combine=c) %dopar% {
     # calculate the sum of the absolute difference
     sum(abs(dat1 - dat2[colPerms[[i]]]))
   }
-  
+
   stopCluster(cl)
-  
+
   # find the index with the lowest overall difference
-  idx <- which(sums == min(sums), arr.ind = TRUE)
+  idx <- which(sums == min(sums), arr.ind = TRUE)[1]
 
   # apply the optimal column order
   sorted2 <- dat2[colPerms[[idx]]]
-  
+
 } else {
-  
+
   # using the k-1 matrix, find the best fit for each column in the k matrix (without replacement)
   # this simple algorithm optimises the order on a per column basis, rather than for the matrix as a whole, i.e. O(n)
   tmp <- dat2
   best <- c()
-  
+
   for (i in names(dat1)) {
     # subtract the current column from the tmp matrix
     sums <- colSums(abs(tmp-dat1[[i]]))
-    
+
     # find the best column match
     idx <- which(sums == min(sums), arr.ind = FALSE)
     best[[i]] <- names(idx)
-    
+
     # delete the column from the matrix
     tmp <- tmp[-idx]
   }
-  
+
   # apply the optimal column order
   sorted2 <- dat2[best]
 }
